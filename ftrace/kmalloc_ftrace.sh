3c70ebaf15d0f15e1705a29c9eae48967de4f605
hyejeong0807 <hyejeong12311@gmail.com>
2024-03-12 15:02:19 +0900

    add: ftrace 스크립트

---
+++ ftrace/kmalloc_ftrace.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo 0 > /sys/kernel/debug/tracing/tracing_on
+sleep 1
+echo "tracing_off"
+
+echo 0 > /sys/kernel/debug/tracing/events/enable
+sleep 1
+echo "events disabled"
+
+echo nop > /sys/kernel/debug/tracing/current_tracer
+sleep 1
+echo "nop tracer enabled"
+
+echo 1 > /sys/kernel/debug/tracing/events/kmem/kmalloc/enable
+echo 1 > /sys/kernel/debug/tracing/events/kmem/kfree/enable
+
+sleep 1
+echo "event enabled"
+
+echo 1 > /sys/kernel/debug/tracing/tracing_on
+echo "tracing_on"
+
+
